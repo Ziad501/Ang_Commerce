cc7c2c394cf989b649580f2e688a03310efa1e1f
mizosaood32 <mizosaood23@gmail.com>
2025-03-27 01:52:32 +0200

    fetch data for SideNav component
    - Created `sampleData` folder for mock data
    - Defined `Category` interface for type safety
    - Implemented `CategoryService` to fetch category data
    - Integrated category data into the `SideNavComponent`

--- src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+
+import { Category } from '../../types/category.type';
 
 @Component({
   selector: 'app-sidenavigation',
@@ -7,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './sidenavigation.component.scss'
 })
 export class SidenavigationComponent {
+  categories: Category[] = [];
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+  }
 
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
 }
