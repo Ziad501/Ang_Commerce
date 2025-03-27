0283e45f0650b42b66301f0f73328ed1481d9c89
mizosaood32 <mizosaood23@gmail.com>
2025-03-27 14:12:27 +0200

    Added ratings feature - Created ratings component in shared module - Updated product.type.ts to include ratings - Imported shared module in home component - Adjusted product.data.ts with ratings values - Updated product.component.html to display ratings

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,12 +6,13 @@ import { CatnavigationComponent } from './components/catnavigation/catnavigation
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
 import { ProductsComponent } from './components/products/products.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [HomeComponent, SidenavigationComponent, ProductsComponent],
   imports: [
     CommonModule,FontAwesomeModule,CatnavigationComponent,HeaderComponent  
-  ]
+  ,SharedModule]
 })
 export class HomeModule { }
